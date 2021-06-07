4a88b2f6723fc3b33ead34c8508b9ee654aef57c
Gabriel Henton <gabehenton@gmail.com>
2021-06-06 20:00:08 -0700

    MVP and all tests pass

--- api/server.js
+++ api/server.js
@@ -1,2 +1,25 @@
 // implement your server here
+const express = require('express');
+const server = express();
 // require your posts router and connect it here
+// BREAK UP THIS MONOLITHIC FILE USING ROUTES
+// BREAK UP THIS MONOLITHIC FILE USING ROUTES
+// BREAK UP THIS MONOLITHIC FILE USING ROUTES
+
+const postsRouter = require("./posts/posts-router.js")
+
+server.use(express.json());
+server.use("/api/posts", postsRouter)
+
+
+// OTHER ENDPOINTS
+// OTHER ENDPOINTS
+// OTHER ENDPOINTS
+server.get('/', (req, res) => {
+  res.send(`
+    <h2>Blog Posts API</h>
+    <p>Welcome to the Blog Posts API</p>
+  `);
+});
+
+module.exports = server;
